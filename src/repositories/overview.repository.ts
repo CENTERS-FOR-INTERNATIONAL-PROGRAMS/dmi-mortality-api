1aebbd87178c44de2a5ba0ed8f4bcf29999af0b4
brianWakhutu <brian.wakhutu@gmail.com>
2023-08-01 17:40:54 +0300

    sorted the db connection to use the object model and not the file

--- src/repositories/overview.repository.ts
+++ src/repositories/overview.repository.ts
@@ -1,16 +1,17 @@
 import { NumberEnrolled } from './../models/numberEnrolled.model';
-import sequlize from "../db/connection";
+
 
 import { QueryTypes } from 'sequelize';
 import { Covid19ByAgeSex } from '../models/covid19ByAgeSex.model';
 import { Covid19OverTime } from '../models/covid19overtime.model';
 import { Covid19PositivityRate } from '../models/covid19Positivity.model';
+import Database from '../db';
 
 
 
 
 interface IOverviewRepository {
-    retrieveNumberEnrolled(): Promise<NumberEnrolled[]>
+    retrieveNumberEnrolledByFacility(): Promise<NumberEnrolled[]>
     retrieveCovid19ByAgeSex(): Promise<Covid19ByAgeSex[]>
     retrieveCovid19OverTime(): Promise<Covid19OverTime[]>
     retrieveCovid19Positivity(): Promise<Covid19PositivityRate[]>
@@ -19,7 +20,12 @@ interface IOverviewRepository {
 
 class OverviewRepository implements IOverviewRepository {
     covid19OVerTime: any;
-    async retrieveNumberEnrolled(): Promise<NumberEnrolled[]> {
+    numberEnrolled : any;
+    covid19ByAgeSex : any;
+    covidPositivityRate : any;
+     db = new Database();
+
+    async retrieveNumberEnrolledByFacility(): Promise<NumberEnrolled[]> {
         let condition = '';
         condition += 'and SampleTested is not null and barcode is not null Group by Facility';
         const bindings: any[] = [];
@@ -27,43 +33,44 @@ class OverviewRepository implements IOverviewRepository {
                         Covid19Positive,Facility  from [dbo].[FactMortality] 
                         Where SampleTested = 1 ${condition};`
 
-        const [results, metadata] = await sequlize.query<NumberEnrolled[]>(query, {
+        this.numberEnrolled = await this.db.sequelize?.query<NumberEnrolled[]>(query, {
             type: QueryTypes.SELECT,
 
         });
-        return results;
+        console.log(this.numberEnrolled)
+        return this.numberEnrolled;
     }
   
+
+
     async retrieveCovid19ByAgeSex(): Promise<Covid19ByAgeSex[]> {
         let condition = '';
         condition += 'and SampleTested is not null and barcode is not null and AgeGroup is not null Group by AgeGroup,sex'
         const query = `SELECT 
                        sum(Covid19Positive) CovidPositive, AgeGroup, sex
                        FROM  [dbo].[FactMortality]  p
-                        WHERE SampleTested = 1 ${condition};`
-         const [results, metadata] = await sequlize.query<Covid19ByAgeSex[]>(query, {
+                       WHERE SampleTested = 1 ${condition};`
+         this.covid19ByAgeSex = await this.db.sequelize?.query<Covid19ByAgeSex[]>(query, {
             type: QueryTypes.SELECT,
 
              });
-              console.log(results);
-        return results;
+        console.log(this.covid19ByAgeSex);
+        return this.covid19ByAgeSex;
 
     }
     async retrieveCovid19OverTime(): Promise<Covid19OverTime[]> {
         
-
-
         let condition = '';
         condition += 'and SampleTested is not null and barcode is not null'
         const query = `SELECT 
         count(SampleTested) SampleTested, 
         sum(Covid19Positive) CovidPositive,
         EpiWeek
-    FROM  [dbo].[FactMortality]  p
-    WHERE SampleTested = 1 ${condition}
-    Group by EpiWeek;`
-         this.covid19OVerTime = await sequlize.query<Covid19OverTime[]>(query, {
-            type: QueryTypes.SELECT,
+        FROM  [dbo].[FactMortality]  p
+        WHERE SampleTested = 1 ${condition}
+        Group by EpiWeek;`
+        this.covid19OVerTime = await this.db.sequelize?.query<Covid19OverTime[]>(query, {
+        type: QueryTypes.SELECT,
 
         });
  
@@ -77,12 +84,12 @@ class OverviewRepository implements IOverviewRepository {
             , Covid19Negative =(select count(N.Covid19Positive)  from [dbo].[FactMortality] N where Covid19Positive = 0 and SampleTested = 1 and SampleTested is not null and barcode is not null)     
             from [dbo].[FactMortality]  t
             Where SampleTested = 1 and SampleTested is not null and barcode is not null ) A`
-         const [results, metadata] = await sequlize.query<Covid19PositivityRate[]>(query, {
+            this.covidPositivityRate = await this.db.sequelize?.query<Covid19PositivityRate[]>(query, {
             type: QueryTypes.SELECT,
 
              });
-              console.log(results);
-                  return results;
+              console.log(this.covidPositivityRate);
+            return this.covidPositivityRate;
     }
 
 }

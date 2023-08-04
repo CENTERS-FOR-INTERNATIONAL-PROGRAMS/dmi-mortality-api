803dc2186c4974e95a004d84eaaaeaf1d9f0652c
gmm2204 <gmm2204@cumc.columbia.edu>
2023-08-04 13:40:17 +0300

    Implemented covid positivity by age gender

--- src/repositories/overview.repository.ts
+++ src/repositories/overview.repository.ts
@@ -1,29 +1,26 @@
 import { NumberEnrolled } from './../models/numberEnrolled.model';
 
-
 import { QueryTypes } from 'sequelize';
 import { Covid19ByAgeSex } from '../models/covid19ByAgeSex.model';
 import { Covid19OverTime } from '../models/covid19overtime.model';
 import { Covid19PositivityRate } from '../models/covid19Positivity.model';
+import { Covid19PositivityByAgeGender } from '../models/covid19PositivityByAgeGender.model';
 import Database from '../db';
-
-
-
 
 interface IOverviewRepository {
     retrieveNumberEnrolledByFacility(): Promise<NumberEnrolled[]>
     retrieveCovid19ByAgeSex(): Promise<Covid19ByAgeSex[]>
     retrieveCovid19OverTime(): Promise<Covid19OverTime[]>
     retrieveCovid19Positivity(): Promise<Covid19PositivityRate[]>
-
 }
 
 class OverviewRepository implements IOverviewRepository {
     covid19OVerTime: any;
-    numberEnrolled : any;
-    covid19ByAgeSex : any;
-    covidPositivityRate : any;
-     db = new Database();
+    numberEnrolled: any;
+    covid19ByAgeSex: any;
+    covidPositivityRate: any;
+    covid19ScreenedByGenderAge: any;
+    db = new Database();
 
     async retrieveNumberEnrolledByFacility(): Promise<NumberEnrolled[]> {
         let condition = '';
@@ -37,10 +34,10 @@ class OverviewRepository implements IOverviewRepository {
             type: QueryTypes.SELECT,
 
         });
-     
+
         return this.numberEnrolled;
     }
-  
+
     async retrieveCovid19ByAgeSex(): Promise<Covid19ByAgeSex[]> {
         let condition = '';
         condition += 'and SampleTested is not null and barcode is not null and AgeGroup is not null Group by AgeGroup,sex'
@@ -48,16 +45,17 @@ class OverviewRepository implements IOverviewRepository {
                        sum(Covid19Positive) CovidPositive, AgeGroup, sex
                        FROM  [dbo].[FactMortality]  p
                        WHERE SampleTested = 1 ${condition};`
-         this.covid19ByAgeSex = await this.db.sequelize?.query<Covid19ByAgeSex[]>(query, {
+        this.covid19ByAgeSex = await this.db.sequelize?.query<Covid19ByAgeSex[]>(query, {
             type: QueryTypes.SELECT,
 
-             });
-      return this.covid19ByAgeSex;
+        });
+        return this.covid19ByAgeSex;
 
 
     }
+
     async retrieveCovid19OverTime(): Promise<Covid19OverTime[]> {
-        
+
         let condition = '';
         condition += 'and SampleTested is not null and barcode is not null'
         const query = `SELECT 
@@ -69,31 +67,48 @@ class OverviewRepository implements IOverviewRepository {
         WHERE SampleTested = 1 ${condition}
         Group by EpiWeek;`
         this.covid19OVerTime = await this.db.sequelize?.query<Covid19OverTime[]>(query, {
-        type: QueryTypes.SELECT,
+            type: QueryTypes.SELECT,
 
         });
- 
-        console.log( this.covid19OVerTime);
 
-        return  this.covid19OVerTime; 
+        console.log(this.covid19OVerTime);
+
+        return this.covid19OVerTime;
     }
+
     async retrieveCovid19Positivity(): Promise<Covid19PositivityRate[]> {
- 
+
         const query = `SELECT Distinct Covid19Positive, Covid19Negative FROM (
             select Covid19Positive=(select count(p.Covid19Positive)  from [dbo].[FactMortality] p where Covid19Positive = 1 and SampleTested = 1 and SampleTested is not null and barcode is not null )
             , Covid19Negative =(select count(N.Covid19Positive)  from [dbo].[FactMortality] N where Covid19Positive = 0 and SampleTested = 1 and SampleTested is not null and barcode is not null)     
             from [dbo].[FactMortality]  t
             Where SampleTested = 1 and SampleTested is not null and barcode is not null ) A`
-            this.covidPositivityRate = await this.db.sequelize?.query<Covid19PositivityRate[]>(query, {
+        this.covidPositivityRate = await this.db.sequelize?.query<Covid19PositivityRate[]>(query, {
             type: QueryTypes.SELECT,
 
-             });
+        });
 
-              console.log(this.covidPositivityRate);
-            return this.covidPositivityRate;
+        console.log(this.covidPositivityRate);
+        return this.covidPositivityRate;
 
     }
 
+    async retrieveCovid19PositivityByAgeGender(): Promise<Covid19PositivityByAgeGender[]> {
+        const query = `SELECT Sum(p.Covid19Positive) As PositiveNumber,
+        (SELECT SexValue from [dbo].[DimSex] where SexId = sex) Gender, 
+        (SELECT AgeGroup from [dbo].[DimAgeGroup] where AgeGroupId = p.AgeGroup) AgeGroup
+        FROM [dbo].[FactMortality] p 
+        WHERE Covid19Positive = 1 and 
+        SampleTested = 1 and SampleTested is not null and barcode is not null
+        GROUP BY sex, AgeGroup`
+
+        this.covid19ScreenedByGenderAge = await this.db.sequelize?.query<Covid19PositivityByAgeGender[]>(query, {
+            type: QueryTypes.SELECT,
+        });
+
+        console.log(this.covid19ScreenedByGenderAge);
+        return this.covid19ScreenedByGenderAge;
+    }
 }
-   
+
 export default new OverviewRepository
--- src/routes/overview.routes.ts
+++ src/routes/overview.routes.ts
@@ -8,13 +8,11 @@ class OverviewRoutes {
     }
     ///checking with leaon
     intializeRoutes() {
-
         this.router.get("/findNumberEnrolledByFacility", this.controller.findNumberEnrolledByFacility);
         this.router.get("/findCovid19ByAgeSex", this.controller.findCovid19ByAgeSex);
         this.router.get("/findCovid19OverTime", this.controller.findCovid19OverTime);
         this.router.get("/findCovidPositivity", this.controller.findCovid19Positivity);
-
-
+        this.router.get("/findCovid19PositivityByAgeGender", this.controller.findCovid19PositivityByAgeGender);
     }
 }
 export default new OverviewRoutes().router;

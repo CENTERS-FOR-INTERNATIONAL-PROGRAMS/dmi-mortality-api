fca6bab20d28c3c7f9c34e372cb45f9aeeb8d7c6
gmm2204 <gmm2204@cumc.columbia.edu>
2023-08-09 11:38:14 +0300

    Updated covid-19 results query

--- src/repositories/covid19/results.repository.ts
+++ src/repositories/covid19/results.repository.ts
@@ -63,7 +63,7 @@ class ResultsRepository implements IResultsRepository {
     async retrieveByPositivityOverTime(): Promise<Covid19Results[]> {
         const query = `SELECT 
         count(SampleTested) SampleTested, 
-        sum(Covid19Positive) CovidPositive,
+        sum(Covid19Positive) Covid19Positive,
         EpiWeek,
         (select  [Month] from [dbo].[DimEpiWeek] where WeekKey = P.EpiWeek ) [Month],
         (select  [Year] from [dbo].[DimEpiWeek] where WeekKey = P.EpiWeek ) [Year]
